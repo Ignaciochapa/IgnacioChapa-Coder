d7e24d86a81738f3710fa71f6800363f3ae05203
Luis Ignacio Chapa <luisignaciochapa@gmail.com>
2022-12-09 20:19:30 -0300

    entFinal

--- main.js
+++ main.js
@@ -95,7 +95,6 @@ const productos = [
 
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
-
 function cargarProductos(productosElegidos) {
     contenedorProductos.innerHTML = "";
     productosElegidos.forEach(producto => {
@@ -118,7 +117,6 @@ function cargarProductos(productosElegidos) {
         })
     })
 }
-
 cargarProductos(productos);
 
 const contenedorCarrito = document.getElementById('carrito-contenedor')
@@ -132,29 +130,19 @@ let carrito = []
 
 //AGREGAR AL CARRITO
 const agregarAlCarrito = (prodId) => {
-
-    //PARA AUMENTAR LA CANTIDAD Y QUE NO SE REPITA
-    const existe = carrito.some (prod => prod.id === prodId) //comprobar si el elemento ya existe en el carro
-
-    if (existe){ //SI YA ESTÁ EN EL CARRITO, ACTUALIZAMOS LA CANTIDAD
-        const prod = carrito.map (prod => { //creamos un nuevo arreglo e iteramos sobre cada curso y cuando
-            // map encuentre cual es el q igual al que está agregado, le suma la cantidad
+    const existe = carrito.some (prod => prod.id === prodId)
+    if (existe){
+        const prod = carrito.map (prod => {
             if (prod.id === prodId){
                 prod.cantidad++
             }
         })
-    } else { //EN CASO DE QUE NO ESTÉ, AGREGAMOS EL CURSO AL CARRITO
-        const item = productos.find((prod) => prod.id === prodId)//Trabajamos con las ID
-        //Una vez obtenida la ID, lo que haremos es hacerle un push para agregarlo al carrito
+    } else {
+        const item = productos.find((prod) => prod.id === prodId)
         carrito.push(item)
     }
-    //Va a buscar el item, agregarlo al carrito y llama a la funcion actualizarCarrito, que recorre
-    //el carrito y se ve.
-    actualizarCarrito() //LLAMAMOS A LA FUNCION QUE CREAMOS EN EL TERCER PASO. CADA VEZ Q SE 
-    //MODIFICA EL CARRITO
+    actualizarCarrito()
 }
-//agregarAlCarrito(1) //Le pasamos el ID por parametro. Tenemos que asigarle como evento esta funcion al boton
-//con el id de su producto correspondiente
 
 // Local Storage
 document.addEventListener('DOMContentLoaded', () => {
@@ -166,14 +154,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Actualizar carrito
 const actualizarCarrito = () => {
-    //4- CUARTO PASO
-    //LOS APPENDS SE VAN ACUMULANDO CON LO QE HABIA ANTES
-    contenedorCarrito.innerHTML = "" //Cada vez que yo llame a actualizarCarrito, lo primero q hago
-    //es borrar el nodo. Y despues recorro el array lo actualizo de nuevo y lo rellena con la info
-    //actualizado
-    //3 - TERCER PASO. AGREGAR AL MODAL. Recorremos sobre el array de carrito.
-
-    //Por cada producto creamos un div con esta estructura y le hacemos un append al contenedorCarrito (el modal)
+    contenedorCarrito.innerHTML = ""
     carrito.forEach((prod) => {
         const div = document.createElement('div')
         div.className = ('productoEnCarrito')
@@ -183,20 +164,11 @@ const actualizarCarrito = () => {
         <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
         <button onclick="eliminarDelCarrito(${prod.id})"><i class="bi bi-trash3-fill"></i></button>
         `
-
         contenedorCarrito.appendChild(div)
-        
         localStorage.setItem('carrito', JSON.stringify(carrito))
-
     })
-    //SEPTIMO PASO
-    contadorCarrito.innerText = carrito.length // actualizamos con la longitud del carrito.
-    //OCTAVO PASO
-    console.log(carrito)
+    contadorCarrito.innerText = carrito.length
     precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
-    //Por cada producto q recorro en mi carrito, al acumulador le suma la propiedad precio, con el acumulador
-    //empezando en 0.
-
 }
 
 // Eliminar del carrito
